5cae8563e7ad7fd66e80454a86a83f3219968dea
Anand Prajapati <anandprajapati8508@gmail.com>
2025-03-19 10:54:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-//else if statements in age in vote dene ka list
-
-let age = 20;
-
- if(age >= 18){
-  console.log("yes can vote");
- } else if( age >= 18){
-   console.log("yes can vote");
- } else if(age <= 18){
-   console.log("you can not vote");
+ let marks = 31;
+ //else if Statements in use in 10th , 12th, result  Marks in CGPa 99.99
+ if(marks >= 90){
+ console.log("A+");
+ } else if(marks >= 70){
+   console.log("A");
+ } else if(marks >= 50){
+  console.log("B");
+ } else if(marks >= 40){
+   console.log("C")
+ } else if(marks >= 30){
+   console.log("D");
+ } else if(marks >= 20-10){
+    console.log("F")
  }
