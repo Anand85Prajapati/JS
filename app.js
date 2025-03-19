8cf119c6749af8c9fdabd54559e635d1fba81e9e
Anand Prajapati <anandprajapati8508@gmail.com>
2025-03-19 10:33:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+ let age = 20;
+
+ if(age >= 18){
+  console.log("yes can vote");
+ } else if( age >= 18){
+   console.log("yes can vote");
+ } else if(age <= 18){
+   console.log("you can not vote");
+ }
